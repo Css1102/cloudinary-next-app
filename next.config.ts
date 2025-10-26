e4f25ec25815cadce0f7c0fd0280dab091dd15c9
Chiranjeev Singh Sethi <chiranjeev@Chiranjeevs-MacBook-Air.local>
2025-10-26 22:26:26 +0530

    twelth nextjs commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+    env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
 };
 
 export default nextConfig;
